0d26b6e414d7ea6635e2979eb8607e151dac0d16
debasishrowlo <debasishrowlo@gmail.com>
2024-02-06 20:29:11 +0530

    Implement tablet layout

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -4,6 +4,9 @@ import { Link, useParams } from "react-router-dom"
 import logo from "@/assets/shared/desktop/logo.svg"
 import hamburgerIcon from "@/assets/shared/tablet/icon-hamburger.svg"
 import cartIcon from "@/assets/shared/desktop/icon-cart.svg"
+import facebookIcon from "@/assets/shared/desktop/icon-facebook.svg"
+import twitterIcon from "@/assets/shared/desktop/icon-twitter.svg"
+import instagramIcon from "@/assets/shared/desktop/icon-instagram.svg"
 
 import data from "@/data.json" 
 
@@ -101,7 +104,9 @@ const Product = () => {
           <div className="flex justify-center md:grow md:justify-start lg:grow-0">
             <img src={logo} />
           </div>
-          <div className="hidden space-x-8 lg:block">
+          <div 
+            className="hidden space-x-8 lg:block"
+          >
             {menuItems.map((category, index) => (
               <a 
                 href="#" 
@@ -123,7 +128,8 @@ const Product = () => {
         </div>
         <div className="container mx-auto mt-6 px-6 md:px-8 md:flex md:items-center md:space-x-[70px] lg:space-x-[125px] lg:px-0">
           <div className="rounded-8 overflow-hidden md:w-2/5 lg:w-1/2">
-            <img src={require(`../assets/product-${product.slug}/mobile/image-product.jpg`)} />
+            <img src={require(`@/assets/product-${product.slug}/mobile/image-product.jpg`)} className="md:hidden" />
+            <img src={require(`@/assets/product-${product.slug}/tablet/image-product.jpg`)} className="hidden md:block" />
           </div>
           <div className="mt-8 md:w-3/5 md:mt-0 md:py-11 lg:w-1/2">
             {product.new && (
@@ -163,64 +169,68 @@ const Product = () => {
           <p>FEATURES</p>
           <p className="whitespace-pre-line">{product.features}</p>
         </div>
-        <p>IN THE BOX</p>
-        <ul>
-          {product.includedProducts.map(((includedProduct, index) => (
-            <li key={index}>
-              {includedProduct.quantity}x {includedProduct.name}
-            </li>
-          )))}
-        </ul>
+        <div className="md:flex">
+          <p className="md:w-1/2">IN THE BOX</p>
+          <ul className="md:w-1/2">
+            {product.includedProducts.map(((includedProduct, index) => (
+              <li key={index}>
+                {includedProduct.quantity}x {includedProduct.name}
+              </li>
+            )))}
+          </ul>
+        </div>
         <div className="md:hidden">
-          <img src={require(`../assets/product-${product.slug}/mobile/image-gallery-1.jpg`)} />
-          <img src={require(`../assets/product-${product.slug}/mobile/image-gallery-2.jpg`)} />
-          <img src={require(`../assets/product-${product.slug}/mobile/image-gallery-3.jpg`)} />
+          <img src={require(`@/assets/product-${product.slug}/mobile/image-gallery-1.jpg`)} />
+          <img src={require(`@/assets/product-${product.slug}/mobile/image-gallery-2.jpg`)} />
+          <img src={require(`@/assets/product-${product.slug}/mobile/image-gallery-3.jpg`)} />
         </div>
         <div className="hidden md:flex">
-          <div className="w-1/2">
-            <img src={require(`../assets/product-${product.slug}/tablet/image-gallery-1.jpg`)} />
-            <img src={require(`../assets/product-${product.slug}/tablet/image-gallery-2.jpg`)} />
+          <div className="w-2/5">
+            <img src={require(`@/assets/product-${product.slug}/tablet/image-gallery-1.jpg`)} />
+            <img src={require(`@/assets/product-${product.slug}/tablet/image-gallery-2.jpg`)} />
           </div>
-          <div className="w-1/2">
-            <img src={require(`../assets/product-${product.slug}/tablet/image-gallery-3.jpg`)} />
+          <div className="w-3/5">
+            <img src={require(`@/assets/product-${product.slug}/tablet/image-gallery-3.jpg`)} />
           </div>
         </div>
         <div>
           <p>You may also like</p>
-          {product.similarProducts.map((similarProduct, index) => {
-            return (
-              <div>
-                <img src={require(`../assets/shared/mobile/image-${similarProduct.slug}.jpg`)} />
-                <p>{similarProduct.name}</p>
-                <Link to={`/products/${similarProduct.slug}`}>SEE PRODUCT</Link>
-              </div>
-            )
-          })}
+          <div className="md:flex">
+            {product.similarProducts.map((similarProduct, index) => {
+              return (
+                <div key={index}>
+                  <img src={require(`@/assets/shared/mobile/image-${similarProduct.slug}.jpg`)} className="md:hidden" />
+                  <img src={require(`@/assets/shared/tablet/image-${similarProduct.slug}.jpg`)} className="hidden md:block" />
+                  <p>{similarProduct.name}</p>
+                  <Link to={`/products/${similarProduct.slug}`}>SEE PRODUCT</Link>
+                </div>
+              )
+            })}
+          </div>
         </div>
-        <img src={require(`../assets/shared/mobile/image-best-gear.jpg`)} />
-        <p>Bringing you the best audio gear</p>
-        <p>Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
-        <div>
+        <div className="md:flex">
           {categories.map((category, index) => {
             return (
-              <div>
-                <img src={require(`../assets/shared/mobile/${category.name.toLowerCase()}.png`)} />
-                {/* <img src={require(`../assets/shared/mobile/speakers.png`)} /> */}
+              <div key={index}>
+                <img src={require(`@/assets/shared/desktop/image-category-thumbnail-${category.name.toLowerCase()}.png`)} />
                 <p>{category.name}</p>
                 <Link to={category.url}>SHOP</Link>
               </div>
             )
           })}
         </div>
+        <img src={require(`@/assets/shared/mobile/image-best-gear.jpg`)} className="md:hidden" />
+        <img src={require(`@/assets/shared/tablet/image-best-gear.jpg`)} className="hidden md:block" />
+        <p>Bringing you the best audio gear</p>
+        <p>Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
       </main>
       <footer className="bg-black">
         <img src={logo} />
-        <ul>
+        <ul className="md:flex">
           {menuItems.map((category, index) => (
-            <li>
+            <li key={index}>
               <Link 
                 to={`/categories/${category.name.toLowerCase()}`}
-                key={index}
                 className="text-white"
               >
                 {category.name}
@@ -229,38 +239,13 @@ const Product = () => {
           ))}
         </ul>
         <p className="text-white">Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.</p>
-        <p className="text-white">Copyright 2021. All Rights Reserved</p>
-        <div className="flex justify-center">
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            strokeWidth={0}
-            viewBox="0 0 448 512"
-            xmlns="http://www.w3.org/2000/svg"
-            className="text-white w-10 h-10"
-          >
-            <path d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z" />
-          </svg>
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            strokeWidth={0}
-            viewBox="0 0 512 512"
-            xmlns="http://www.w3.org/2000/svg"
-            className="text-white w-10 h-10"
-          >
-            <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" />
-          </svg>
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            strokeWidth={0}
-            viewBox="0 0 448 512"
-            xmlns="http://www.w3.org/2000/svg"
-            className="text-white w-10 h-10"
-          >
-            <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
-          </svg>
+        <div className="md:flex">
+          <p className="text-white">Copyright 2021. All Rights Reserved</p>
+          <div className="flex justify-center">
+            <img src={facebookIcon} />
+            <img src={twitterIcon} />
+            <img src={instagramIcon} />
+          </div>
         </div>
       </footer>
     </>
